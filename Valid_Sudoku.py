a9b5860d1d2efdf45309ccc86330ee5936d7465a
Shreeya <121918827+ShreeyaPai@users.noreply.github.com>
2023-12-03 23:11:57 +0530

    Update Valid_Sudoku.py

--- Valid_Sudoku.py
+++ Valid_Sudoku.py
@@ -4,13 +4,17 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        res=[]
+        row=[]
+        column=[]
+        grid=[]
         for i in range(9):
             for j in range(9):
                 element = board[i][j]
                 if element != '.':
-                    res += [(i, element), (element, j), (i // 3, j // 3, element)]
-        print(res)
-        return len(res) == len(set(res))
+                    row+=[(i,element)]
+                    column+=[(j,element)]
+                    grid+=[(i//3,j//3,element)]
+        print(row,column,grid)
+        return len(row)==len(set(row)) and len(column)==len(set(column)) and len(grid)==len(set(grid))
 
                 
