da24b081812556174d710e657a9cc6ffe9c03b7e
Shreeya <121918827+ShreeyaPai@users.noreply.github.com>
2024-03-29 12:17:36 +0530

    Update Reorder List.py

--- Reorder List.py
+++ Reorder List.py
@@ -1,24 +1,36 @@
-class Solution:
-    def reorderList(self, head: ListNode) -> None:
-        # find middle
-        slow, fast = head, head.next
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reorderList(self, head):
+        """
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
+        """
+        slow,fast=head,head.next
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+            slow=slow.next
+            fast=fast.next.next
 
-        # reverse second half
-        second = slow.next
-        prev = slow.next = None
+        second=slow.next
+        prev=slow.next=None
+        
         while second:
-            tmp = second.next
-            second.next = prev
-            prev = second
-            second = tmp
+            temp=second.next
+            second.next=prev
+            prev=second
+            second=temp
+        
+        # print(fast)
+        first,last=head,prev
+        while last:
+            ltemp,rtemp=first.next,last.next
+            first.next=last
+            last.next=ltemp
+            first,last=ltemp,rtemp
+        #print(head)
+            
 
-        # merge two halfs
-        first, second = head, prev
-        while second:
-            tmp1, tmp2 = first.next, second.next
-            first.next = second
-            second.next = tmp1
-            first, second = tmp1, tmp2
+        
