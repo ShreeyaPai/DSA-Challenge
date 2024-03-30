600773434b015c317361d3d488926c7a7ea294d3
Shreeya <121918827+ShreeyaPai@users.noreply.github.com>
2024-03-30 23:10:06 +0530

    Update Lowest Common Ancestor of a Binary Search Tree.py

--- Lowest Common Ancestor of a Binary Search Tree.py
+++ Lowest Common Ancestor of a Binary Search Tree.py
@@ -16,3 +16,26 @@ class Solution(object):
         if(root.val > p.val and root.val > q.val): return self.lowestCommonAncestor(root.left, p, q)
         elif(root.val < p.val and root.val < q.val): return self.lowestCommonAncestor(root.right, p, q)
         else: return root
+
+#######   ITERATIVE APPROACH ############
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        cur=root
+        while cur:
+            if(cur.val > p.val and cur.val > q.val): cur=cur.left
+            elif(cur.val < p.val and cur.val < q.val): cur=cur.right
+            else: return cur
